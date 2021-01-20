cef2135245de1a319179183fea12c0cbf07d5b78
Amber Race <amber.race@outlook.com>
2021-01-19 23:16:24 -0800

    completed part 1

--- aoc_2019/src/main/scala/Day10.scala
+++ aoc_2019/src/main/scala/Day10.scala
@@ -6,32 +6,108 @@ import scala.collection.mutable.HashMap
 object Day10 extends Day {
 
   override var day: Int = 10
-
-  def getAsteroids(field:List[String]):List[Coordinate] = {
+  def getAsteroids(field:List[String]):(Int, List[Coordinate]) = {
+      val size = field.length
       val list = ListBuffer[Coordinate]()
-      for(y <- 0 until field.length){
-          val line = field(y).toCharArray
-          for(x <- 0 until line.length){
-              if (line(x) == '#'){
-                  list += Coordinate(x,y)
-              }
+      for(y <- 0 until size){
+        val line = field(y).toCharArray
+        for(x <- 0 until size){
+          if (line(x) != '.'){
+            list += Coordinate(x,y)
           }
+        }
       }
-      list.toList
+      (size, list.toList)
     }
 
-  def findMonitorLocation(field:List[String]):(Coordinate, Int) = {
-      val asteroids = getAsteroids(field)
-      val m = HashMap[Coordinate, List[Coordinate]]()
-      asteroids.foreach(a => {
-          if(!m.contains(a)){
-              m += (a -> List[Coordinate]())
-          }
-          
+  final val TOP_LEFT = 1
+  final val TOP_RIGHT = 2
+  final val BOTTOM_LEFT = 3
+  final val BOTTOM_RIGHT = 4
 
+  def getLinesOfSight(a:Coordinate, size:Int, quadrant:Int):List[List[(Int,Int)]]={
+    val los = ListBuffer[List[(Int,Int)]]()
+    val x_range = {
+        quadrant match {
+          case TOP_LEFT | BOTTOM_LEFT => (a.x - 1 to 0 by -1)
+          case TOP_RIGHT | BOTTOM_RIGHT => (a.x until size)
+        }
+      }
+      val y_range = {
+        quadrant match {
+          case TOP_LEFT | TOP_RIGHT => (a.y to 0 by -1)
+          case BOTTOM_LEFT | BOTTOM_RIGHT => (a.y + 1 until size)
+        }
+      }
+    if (quadrant == TOP_LEFT || quadrant == TOP_RIGHT){
+      los += {
+        (for(x <- x_range) yield (x, a.y)).toList
+      }
+    }
+    if (quadrant == TOP_RIGHT || quadrant == BOTTOM_RIGHT){
+      los += {
+        (for(y <- y_range) yield (a.x, y)).toList
+      }
+    }
+    (1 to x_range.size).foreach(x_step => {
+      val x_list = {
+        quadrant match {
+          case TOP_LEFT | BOTTOM_LEFT => (a.x - x_step to 0 by -(x_step))
+          case TOP_RIGHT | BOTTOM_RIGHT => (a.x + x_step until size by x_step)
+        }
+      }
+      (1 to y_range.size).foreach(y_step => {
+        val y_list = {
+          quadrant match {
+            case TOP_LEFT | TOP_RIGHT => (a.y - y_step to 0 by -(y_step))
+            case BOTTOM_LEFT | BOTTOM_RIGHT => (a.y + y_step until size by y_step)
+          }
+        }
+        los += x_list.toList.zip(y_list.toList)
       })
-      (Coordinate(0,0), 0)
+    })
+    los.toList
   }
+  def scanQuadrant(asteroids:List[Coordinate], a:Coordinate, size:Int, quadrant:Int):Set[Coordinate] = {
+    val los = getLinesOfSight(a, size, quadrant)
+    val seen = ListBuffer[Coordinate]()
+    val blocked = ListBuffer[Coordinate](a)
+    los.foreach(line => {
+      var block = false
+      line.foreach(n => {
+        val c = Coordinate(n._1,n._2)
+        if(block){
+          blocked += c
+        }
+        else if (asteroids.contains(c) && !seen.contains(c) && !blocked.contains(c)) {
+          seen += c
+          block = true
+        }
+      })
+    })
+    seen.toSet
+  }
+
+  def getTotalSeen(asteroids:List[Coordinate], a:Coordinate, size:Int):Set[Coordinate] = {
+    (1 to 4).map(q => scanQuadrant(asteroids, a, size, q)).flatten.toSet
+  }
+
+  def findMonitorLocation(field:List[String]):(Coordinate, Int) = {
+      val (size, asteroids) = getAsteroids(field)
+      var best = Coordinate(0,0)
+      var max_seen = 0
+      asteroids.foreach(a => {
+        val seen = getTotalSeen(asteroids, a, size)
+        if (seen.size > max_seen) {
+          max_seen = seen.size
+          best = a
+        }
+      })
+      (best, max_seen)
+  }
+
+  val (monitor, count) = findMonitorLocation(getInputStrings)
+  printPartOne(count) //286
 
 }
 
--- aoc_2019/src/test/scala/Day10Test.scala
+++ aoc_2019/src/test/scala/Day10Test.scala
@@ -3,14 +3,146 @@ import graph.Coordinate
 
 class Day10Test extends AnyFunSpec {
     describe("Get Asteroids from Input"){
-        it("works with the small example"){
+        describe("works with the small example"){
             val field = List(".#..#",
                              ".....",
                              "#####",
                              "....#",
                              "...##")
+            val (size, asteroids) = Day10.getAsteroids(field)
+            it("gets the correct number of asteroids"){
+              assert(asteroids.length == 10)
+            }
+            it("sets the size correctly"){
+              assert(size == 5)
+            }
+        }
+        describe("works with a medium example"){
+            val field = List(   "......#.#.",
+                                "#..#.#....",
+                                "..#######.",
+                                ".#.#.###..",
+                                ".#..#.....",
+                                "..#....#.#",
+                                "#..#....#.",
+                                ".##.#..###",
+                                "##...#..#.",
+                                ".#....####")
+            val (size, asteroids) = Day10.getAsteroids(field)
+            it("gets the correct number of asteroids"){
+              assert(asteroids.length == 40)
+            }
+            it("sets the size correctly"){
+              assert(size == 10)
+            }
 
-            assert(Day10.getAsteroids(field).length == 10)
+        }
+    }
+
+    describe("Scan Quadrant: Small"){
+        val field = List(".#..#",
+                         ".....",
+                         "#####",
+                         "....#",
+                         "...##")
+        val (size, asteroids) = Day10.getAsteroids(field)
+        val a = Coordinate(2,2)
+        it("works for top left"){
+            val seen = Day10.scanQuadrant(asteroids, a, size, Day10.TOP_LEFT)
+            assert(seen.size == 2)
+            assert(seen.contains(Coordinate(1,0)))
+            assert(seen.contains(Coordinate(1,2)))
+            assert(!seen.contains(Coordinate(0,2)))
+        }
+        it("works for bottom left"){
+            val seen = Day10.scanQuadrant(asteroids, a, size, Day10.BOTTOM_LEFT)
+            assert(seen.size == 0)
+        }
+        it("works for top right"){
+            val seen = Day10.scanQuadrant(asteroids, a, size, Day10.TOP_RIGHT)
+            assert(seen.size == 2)
+            assert(seen.contains(Coordinate(3,2)))
+            assert(seen.contains(Coordinate(4,0)))
+            assert(!seen.contains(Coordinate(4,2)))
+        }
+        it("works for bottom right"){
+            val seen = Day10.scanQuadrant(asteroids, a, size, Day10.BOTTOM_RIGHT)
+            assert(seen.size == 3)
+            assert(seen.contains(Coordinate(4,3)))
+            assert(seen.contains(Coordinate(4,4)))
+            assert(seen.contains(Coordinate(3,4)))
+        }
+        describe("works for top right corner"){
+            val tl = Coordinate(4,0)
+            it("top left qudrant ok"){
+                val seen = Day10.scanQuadrant(asteroids, tl, size, Day10.TOP_LEFT)
+                assert(seen.size == 1)
+            }
+            it("top right qudrant ok"){
+                val seen = Day10.scanQuadrant(asteroids, tl, size, Day10.TOP_RIGHT)
+                assert(seen.size == 0)
+            }
+            it("bottom right qudrant ok"){
+                val seen = Day10.scanQuadrant(asteroids, tl, size, Day10.BOTTOM_RIGHT)
+                assert(seen.size == 1)
+            }
+            it("bottom left qudrant ok"){
+                val seen = Day10.scanQuadrant(asteroids, tl, size, Day10.BOTTOM_LEFT)
+                assert(seen.size == 5)
+            }
+        }
+    }
+
+    describe("Scan Quadrant: Medium"){
+        it("calculates diagonal blocks"){
+            val field = List(
+                "#.........",
+                "...A......",
+                "...B..a...",
+                ".EDCG....a",
+                "..F.c.b...",
+                ".....c....",
+                "..efd.c.gb",
+                ".......c..",
+                "....f...c.",
+                "...e..d..c"
+            )
+            val (size, asteroids) = Day10.getAsteroids(field)
+            val a = Coordinate(0,0)
+            val seen = Day10.scanQuadrant(asteroids, a, size, Day10.BOTTOM_RIGHT)
+            assert(seen.size == 7)
+        }
+    }
+    describe("Get Total Seen"){
+        val field = List(".7..7",
+                         ".....",
+                         "67775",
+                         "....7",
+                         "...87")
+        val (size, asteroids) = Day10.getAsteroids(field)
+        val five = Coordinate(4,2)
+        val six = Coordinate(0,2)
+        val sevens = List(Coordinate(1,0), Coordinate(4,0),
+        Coordinate(1,2),Coordinate(2,2),Coordinate(3,2),
+        Coordinate(4,3),Coordinate(4,4))
+        val eight = Coordinate(3,4)
+        it(s"calculates $five correctly"){
+            val seen = Day10.getTotalSeen(asteroids, five, size)
+            assert(seen.size == 5)
+        }
+        it(s"calculates $six correctly"){
+            val seen = Day10.getTotalSeen(asteroids, six, size)
+            assert(seen.size == 6)
+        }
+        sevens.foreach(s => {
+            it(s"calculates $s correctly"){
+                val seen  = Day10.getTotalSeen(asteroids, s, size)
+                assert(seen.size == 7)
+            }
+        })
+        it(s"calculates $eight correctly"){
+            val seen = Day10.getTotalSeen(asteroids, eight, size)
+            assert(seen.size == 8)
         }
     }
 
@@ -22,9 +154,84 @@ class Day10Test extends AnyFunSpec {
                              "....#",
                              "...##")
 
-            val ml = Day10.findMonitorLocation(field)
-            assert(ml._1 == Coordinate(3,4))
-            assert(ml._2 == 8)
+            val (loc, seen) = Day10.findMonitorLocation(field)
+            assert(loc == Coordinate(3,4))
+            assert(seen == 8)
+        }
+        it("works with medium example 1"){
+            val field = List(   "......#.#.",
+                                "#..#.#....",
+                                "..#######.",
+                                ".#.#.###..",
+                                ".#..#.....",
+                                "..#....#.#",
+                                "#..#....#.",
+                                ".##.#..###",
+                                "##...#..#.",
+                                ".#....####")
+
+            val (loc, seen) = Day10.findMonitorLocation(field)
+            assert(loc == Coordinate(5,8))
+            assert(seen == 33)
+        }
+        it("works with medium example 2"){
+            val field = List(   "#.#...#.#.",
+                                ".###....#.",
+                                ".#....#...",
+                                "##.#.#.#.#",
+                                "....#.#.#.",
+                                ".##..###.#",
+                                "..#...##..",
+                                "..##....##",
+                                "......#...",
+                                ".####.###.")
+
+            val (loc, seen) = Day10.findMonitorLocation(field)
+            assert(loc == Coordinate(1,2))
+            assert(seen == 35)
+        }
+        it("works with medium example 3"){
+            val field = List(   ".#..#..###",
+                                "####.###.#",
+                                "....###.#.",
+                                "..###.##.#",
+                                "##.##.#.#.",
+                                "....###..#",
+                                "..#.#..#.#",
+                                "#..#.#.###",
+                                ".##...##.#",
+                                ".....#.#..")
+
+            val (loc, seen) = Day10.findMonitorLocation(field)
+            assert(loc == Coordinate(6,3))
+            assert(seen == 41)
+        }
+        it("works with large example"){
+            val field = List(   
+                ".#..##.###...#######",
+                "##.############..##.",
+                ".#.######.########.#",
+                ".###.#######.####.#.",
+                "#####.##.#.##.###.##",
+                "..#####..#.#########",
+                "####################",
+                "#.####....###.#.#.##",
+                "##.#################",
+                "#####.##.###..####..",
+                "..######..##.#######",
+                "####.##.####...##..#",
+                ".#####..#.######.###",
+                "##...#.##########...",
+                "#.##########.#######",
+                ".####.#.###.###.#.##",
+                "....##.##.###..#####",
+                ".#.#.###########.###",
+                "#.#.#.#####.####.###",
+                "###.##.####.##.#..##")
+
+            val (loc, seen) = Day10.findMonitorLocation(field)
+            assert(loc == Coordinate(11,13))
+            assert(seen == 210)
         }
     }
 }
