8a76179525c0c38bd29facad8dd39d159b4f7891
Amber Race <amber.race@outlook.com>
2021-01-21 17:13:57 -0800

    Fixed scoring bug for 13.2

--- aoc_2019/src/main/scala/Day13.scala
+++ aoc_2019/src/main/scala/Day13.scala
@@ -4,7 +4,9 @@ import scala.collection.mutable.ListBuffer
 
 object Day13 extends Day {
   override var day: Int = 13
+
   def load(program:Array[Long]):IntcodeComputer = IntcodeComputer(program)
+
   def countBlocks():Int = {
     val icc = load(getInputAsLongArray)
     val output = icc.compute(0,true).split(",")
@@ -15,57 +17,58 @@ object Day13 extends Day {
     })
     blocks
 }
-def gameFromOutput(output:List[Int]):Game = {
-  val blocks = ListBuffer[Coordinate]()
-  val walls = ListBuffer[Coordinate]()
-  var paddle = Coordinate()
-  var balls = ListBuffer[Coordinate]()
-  var empty = ListBuffer[Coordinate]()
-  var score = 0
-  (0 until output.length - 2 by 3).foreach(i => {
-    val x = output(i)
-    val y = output(i+1)
-    val t = output(i+2)
-    if (x == -1 && y == 0) score = t 
-    else {
-      t match {
-        case 0 => empty += Coordinate(x,y)
-        case 1 => walls += Coordinate(x,y)
-        case 2 => blocks += Coordinate(x,y)
-        case 3 => paddle = Coordinate(x,y)
-        case 4 => balls += Coordinate(x,y)
-        case _ => true
-      }
-    }
-  })
-  
-  new Game(walls.toSet.toList, 
-          blocks.toSet.diff(empty.toSet).diff(balls.toSet).toList, 
-          paddle, balls.last, score)
-}
 
 def playGame():Int = {
   val program = getInputAsLongArray
   program.update(0, 2L)
   val icc = load(program)
-  var score = 0
+
+  val blocks = ListBuffer[Coordinate]()
+  val walls = ListBuffer[Coordinate]()
+  var paddle = Coordinate()
+  val balls = ListBuffer[Coordinate]()
+  val empty = ListBuffer[Coordinate]()
+
   var blockCount = Int.MaxValue
-  var boardSize = -1
   var joystick = 0
+  var outputStart = 0
+  var score = 0
 
   while(blockCount > 0){
     icc.input(joystick)
-    val output = icc.compute(icc.currentPointer,true)
-                    .split(",")
+    val output = icc.compute(icc.currentPointer,true).split(",")
+    // intcode outputs everything each time but we
+    // only need to update with the latest data
+    val trimmed = output.slice(outputStart, output.length)
                     .map(l => l.toInt)
                     .toList
-    if(boardSize < 0) boardSize = output.length
-    val game = gameFromOutput(output.slice(output.length - boardSize,output.length))
+    outputStart = output.length //use this next time around
+    (0 until trimmed.length - 2 by 3).foreach(i => {
+        val x = trimmed(i)
+        val y = trimmed(i+1)
+        val t = trimmed(i+2)
+        if (x == -1 && y == 0) score = t 
+        else {
+          t match {
+            case 0 => empty += Coordinate(x,y)
+            case 1 => walls += Coordinate(x,y)
+            case 2 => blocks += Coordinate(x,y)
+            case 3 => paddle = Coordinate(x,y)
+            case 4 => balls += Coordinate(x,y)
+            case _ => true
+          }
+        }
+      })
+
+    val game = new Game(walls.toList, 
+          //need to remove any blocks that have been destroyed
+          blocks.toSet.diff(empty.toSet).diff(balls.toSet).toList, 
+          paddle, balls.last)
     joystick = game.nextMove()
     blockCount = game.blockCount
-    score = game.score
-    game.printScreen()
-    //println(s"blocks: $blockCount,  score: $score")
+    
+    //println("output size: " + output.length + s", trimmed length: " + trimmed.length + s", blocks: $blockCount,  score: $score")
+    //game.printScreen()
   }
   score
 }
@@ -73,7 +76,7 @@ def playGame():Int = {
   printPartTwo(playGame())
 }
 
-case class Game(walls:List[Coordinate], blocks:List[Coordinate], paddle:Coordinate, ball:Coordinate, score:Int){
+case class Game(walls:List[Coordinate], blocks:List[Coordinate], paddle:Coordinate, ball:Coordinate){
   
   def height:Int = walls.map(w => w.y).max
   def width:Int = walls.map(w => w.x).max
@@ -94,8 +97,7 @@ case class Game(walls:List[Coordinate], blocks:List[Coordinate], paddle:Coordina
     println('\n')
   }
   def nextMove():Int = {
-    if (blockCount == 0) 0
-    else if (ball.x > paddle.x) 1
+    if (ball.x > paddle.x) 1
     else if (ball.x < paddle.x) -1
     else 0
   }
