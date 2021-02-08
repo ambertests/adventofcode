d801f3bc75aa70e3f659eb7e7af0b58af72757a8
Amber Race <amber.race@outlook.com>
2021-02-07 23:03:48 -0800

    Day 10 solution

---
+++ aoc_2017/src/main/java/com/ambertests/aoc/days/Day10.java
@@ -0,0 +1,134 @@
+package com.ambertests.aoc.days;
+
+import com.ambertests.aoc.common.Day;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.google.common.primitives.Ints;
+
+public class Day10 extends Day {
+    public Day10(){
+        this.dayNum = 10;
+    }
+
+    int[] shift(int[] arr, int offset){
+        if(offset == 0){
+            return arr;
+        }
+        int[] shifted = new int[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            int idx = i + offset;
+            if(idx >= arr.length){
+                idx -= arr.length;
+            }
+            if(idx < 0){
+                idx += arr.length;
+            }
+            shifted[idx] = arr[i];
+        }
+        return shifted;
+    }
+
+    int[] reverseSubArray(int[] arr, int start, int len){
+        if(len <= 1){
+            return arr;
+        }
+        int[] reversed = new int[arr.length];
+        int offset = 0 - start;
+        int[] shifted = shift(arr, offset);
+        List<Integer> slice = Ints.asList(Arrays.copyOfRange(shifted, 0, len));
+        Collections.reverse(slice);
+        for(int i = 0; i < arr.length; i++){
+            if(i < slice.size()){
+                reversed[i] = slice.get(i);
+            }else{
+                reversed[i] = shifted[i];
+            }
+        }
+        return shift(reversed, -(offset));
+    }
+
+    int[] processArray(int[] arr, int[] lengths, int loops){
+        int start = 0;
+        int skip = 0;
+        for(int l = 0; l < loops; l++){
+            for(int len:lengths){
+                arr = reverseSubArray(arr, start, len);
+                start += (len + skip);
+                while(start >= arr.length){
+                    start -= arr.length;
+                }
+                skip += 1;
+            }
+        }
+        return arr;
+    }
+
+    int[] initializeArray(){
+        int[] arr = new int[256];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = i;
+        }
+        return arr;
+    }
+
+    int[] getLengths(String str){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(char c:str.toCharArray()){
+            list.add((int)c);
+        }
+        list.addAll(Ints.asList(17, 31, 73, 47, 23));
+        return Ints.toArray(list);
+    }
+
+    String createHash(int[] arr){
+        StringBuilder sb = new StringBuilder();
+        int x = -1;
+        for(int i = 0; i < arr.length; i++){
+            if(i%16 == 0){
+                if(i > 0){
+                    String hex = Integer.toHexString(x);
+                    if(hex.length() == 1){
+                        sb.append("0");
+                    }
+                    sb.append(hex);
+                }
+                x = arr[i];
+            }else{
+                x ^= arr[i];
+            }
+        }
+        String hex = Integer.toHexString(x);
+        if(hex.length() == 1){
+            sb.append("0");
+        }
+        sb.append(hex);
+        return sb.toString();
+    }
+
+    String calculateHash(String lenStr){
+        int[] arr = initializeArray();
+        int[] lengths = getLengths(lenStr);
+        int[] processed = processArray(arr, lengths, 64);
+        return createHash(processed);
+    }
+
+    @Override
+    public void solve() {
+        int[] arr = initializeArray();
+        int[] lengths = new int[]{31,2,85,1,80,109,35,63,98,255,0,13,105,254,128,33};
+        int[] processed = processArray(arr, lengths, 1);
+        this.solution1 = processed[0] * processed[1];
+
+        String lenStr = "31,2,85,1,80,109,35,63,98,255,0,13,105,254,128,33";
+        this.solution2 = calculateHash(lenStr);
+    }
+
+    public static void main(String[] args) {
+        Day10 day = new Day10();
+        day.solve();
+        day.printSolutions();        
+    }
+}
---
+++ aoc_2017/src/test/java/com/ambertests/aoc/days/Day10Test.java
@@ -0,0 +1,109 @@
+package com.ambertests.aoc.days;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Day10Test {
+    @Test
+    public void testShiftArrayRight(){
+        Day10 day = new Day10();
+        int[] arr = new int[]{1,2,3,4,5};
+        int[] shifted = day.shift(arr, 1);
+        int[] expected = new int[]{5,1,2,3,4};
+        assertArrayEquals(expected, shifted);
+    }
+    @Test
+    public void testShiftArrayLeft(){
+        Day10 day = new Day10();
+        int[] arr = new int[]{1,2,3,4,5};
+        int[] shifted = day.shift(arr, -1);
+        int[] expected = new int[]{2,3,4,5,1};
+        assertArrayEquals(expected, shifted);
+    }
+
+    @Test
+    public void testReverseSubArray1(){
+        Day10 day = new Day10();
+        int[] arr = new int[]{0, 1, 2, 3, 4};
+        int[] reversed = day.reverseSubArray(arr, 0, 3);
+        int[] expected = new int[]{2, 1, 0, 3, 4};
+        assertArrayEquals(expected, reversed);
+    }
+    @Test
+    public void testReverseSubArray2(){
+        Day10 day = new Day10();
+        int[] arr = new int[]{2, 1, 0, 3, 4};
+        int[] reversed = day.reverseSubArray(arr, 3, 4);
+        int[] expected = new int[]{4, 3, 0, 1, 2};
+        assertArrayEquals(expected, reversed);
+    }
+    @Test
+    public void testReverseSubArray3(){
+        Day10 day = new Day10();
+        int[] arr = new int[]{4, 3, 0, 1, 2};
+        int[] reversed = day.reverseSubArray(arr, 4, 1);
+        int[] expected = new int[]{4, 3, 0, 1, 2};
+        assertArrayEquals(expected, reversed);
+    }
+    @Test
+    public void testReverseSubArray4(){
+        Day10 day = new Day10();
+        int[] arr = new int[]{4, 3, 0, 1, 2};
+        int[] reversed = day.reverseSubArray(arr, 1, 5);
+        int[] expected = new int[]{3, 4, 2, 1, 0};
+        assertArrayEquals(expected, reversed);
+    }
+
+    @Test
+    public void testProcessArray(){
+        Day10 day = new Day10();
+        int[] arr = new int[]{0, 1, 2, 3, 4};
+        int[] lengths = new int[]{3, 4, 1, 5};
+        int[] expected = new int[]{3, 4, 2, 1, 0};
+        assertArrayEquals(expected, day.processArray(arr, lengths, 1));
+    }
+
+    @Test
+    public void testGetLengths(){
+        Day10 day = new Day10();
+        String str = "1,2,3";
+        int[] expected = new int[]{49,44,50,44,51,17,31,73,47,23};
+        assertArrayEquals(expected, day.getLengths(str));
+    }
+
+    @Test
+    public void testCreateHash(){
+        Day10 day = new Day10();
+        int[] arr = new int[]{65,27,9,1,4,3,40,50,91,7,6,0,2,5,68,22};
+        assertEquals("40", day.createHash(arr));
+    }
+
+    @Test
+    public void testCalculateHash1(){
+        Day10 day = new Day10();
+        String lenStr = "";
+        String expected = "a2582a3a0e66e6e86e3812dcb672a272";
+        assertEquals(expected, day.calculateHash(lenStr));
+    }
+    @Test
+    public void testCalculateHash2(){
+        Day10 day = new Day10();
+        String lenStr = "AoC 2017";
+        String expected = "33efeb34ea91902bb2f59c9920caa6cd";
+        assertEquals(expected, day.calculateHash(lenStr));
+    }
+    @Test
+    public void testCalculateHash3(){
+        Day10 day = new Day10();
+        String lenStr = "1,2,3";
+        String expected = "3efbe78a8d82f29979031a4aa0b16a9d";
+        assertEquals(expected, day.calculateHash(lenStr));
+    }
+    @Test
+    public void testCalculateHash4(){
+        Day10 day = new Day10();
+        String lenStr = "1,2,4";
+        String expected = "63960835bcdc130f0b66d7ff4f6a5a8e";
+        assertEquals(expected, day.calculateHash(lenStr));
+    }
+}
