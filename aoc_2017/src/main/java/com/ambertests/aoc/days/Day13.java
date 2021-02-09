448a5813e867fe5dc31a6bd6d68bd237f25fe897
Amber Race <amber.race@climate.com>
2021-02-08 17:00:52 -0800

    update Day 13 with faster solution

--- aoc_2017/src/main/java/com/ambertests/aoc/days/Day13.java
+++ aoc_2017/src/main/java/com/ambertests/aoc/days/Day13.java
@@ -3,121 +3,56 @@ package com.ambertests.aoc.days;
 import com.ambertests.aoc.common.Day;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Day13 extends Day {
 
-    static class Scanner {
-        int range;
-        boolean increaseDepth;
-        int depth;
-
-        Scanner(int range) {
-            this.range = range;
-            this.increaseDepth = true;
-            this.depth = 0;
-        }
-
-        void move() {
-            if (this.range > 0) {
-                if (this.increaseDepth) {
-                    if (this.depth == range - 1) {
-                        this.increaseDepth = false;
-                    }
-                } else {
-                    if (this.depth == 0) {
-                        this.increaseDepth = true;
-                    }
-                }
-                if(this.increaseDepth){
-                    this.depth += 1;
-                }else{
-                    this.depth -= 1;
-                }
-            }
-        }
-    }
-
-    public Day13() {
-        this.dayNum = 13;
-    }
-
-    int getMaxLayer(String[] input) {
-        return Arrays.stream(input)
-                .map(s -> Integer.parseInt(s.split(": ")[0]))
-                .max(Integer::compare)
-                .orElse(0);
-    }
-
-    Scanner[] initFirewall(String[] input) {
-        Scanner[] firewall = new Scanner[getMaxLayer(input) + 1];
-        Arrays.fill(firewall, new Scanner(0));
-        for (String s : input) {
-            //0: 3
-            String[] lr = s.split(": ");
-            firewall[Integer.parseInt(lr[0])] = new Scanner(Integer.parseInt(lr[1]));
-        }
-        return firewall;
-    }
-
-    int traverseFirewall(Scanner[] firewall){
-        return traverseFirewall(firewall, 0, false);
-    }
-
-    int traverseFirewall(Scanner[] firewall, int delay, boolean stopOnCatch){
-        for(int i = 0; i < delay; i++){
-            Arrays.stream(firewall).forEach(Scanner::move);
-        }
-        int score = 0;
-        int position = 0;
+    int getScore(HashMap<Integer, Integer> firewall, int delay, boolean stopOnCaught) {
         boolean caught = false;
-        while(position < firewall.length){
-            Scanner scanner = firewall[position];
-            if(scanner.depth == 0 && scanner.range > 0){
+        int score = 0;
+        for (int level : firewall.keySet()) {
+            int range = firewall.get(level);
+            if ((delay + level) % ((range - 1) * 2) == 0) {
                 caught = true;
-                score += (position * scanner.range);
-                if(stopOnCatch){
-                    position = firewall.length;
+                score += (level * range);
+                if (stopOnCaught) {
+                    break;
                 }
             }
-            if(!stopOnCatch || !caught) {
-                Arrays.stream(firewall).forEach(Scanner::move);
-                position += 1;
-            }
         }
-        if(caught && score == 0){
-            //penalize runs that were caught only on the first layer
-            score += 1;
+        if (caught && score == 0) {
+            score = 1;
         }
         return score;
     }
 
-    int findDelay(Scanner[] firewall){
+    int findDelay(HashMap<Integer, Integer> firewall) {
         int delay = 0;
-        int score = traverseFirewall(firewall, delay, true);
-        while(score != 0){
+        int score = getScore(firewall, delay, true);
+        while (score != 0) {
             delay += 1;
-            if(delay%10000 == 0){
-                System.out.printf("delay: %d%n", delay);
-            }
-            Arrays.stream(firewall).forEach(scanner -> {
-                scanner.depth = 0;
-                scanner.increaseDepth = true;
-            });
-            score = traverseFirewall(firewall, delay, true);
+            score = getScore(firewall, delay, true);
         }
         return delay;
     }
 
+    HashMap<Integer, Integer> parseInput(String[] input) {
+        HashMap<Integer, Integer> firewall = new HashMap<>();
+        for (String s : input) {
+            //0: 3
+            int[] lr = Arrays.stream(s.split(": "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            firewall.put(lr[0], lr[1]);
+        }
+        return firewall;
+    }
+
     @Override
     public void solve() {
-        Scanner[] firewall = initFirewall(getInputStringArray());
-        this.solution1 = traverseFirewall(firewall);
-        Arrays.stream(firewall).forEach(scanner -> {
-            scanner.depth = 0;
-            scanner.increaseDepth = true;
-        });
-
-        //this.solution2 = findDelay(firewall);
+        HashMap<Integer, Integer> firewall = parseInput(getInputStringArray());
+        this.solution1 = getScore(firewall, 0, false);
+        this.solution2 = findDelay(firewall);
     }
 
     public static void main(String[] args) {
--- aoc_2017/src/test/java/com/ambertests/aoc/days/Day13Test.java
+++ aoc_2017/src/test/java/com/ambertests/aoc/days/Day13Test.java
@@ -2,8 +2,9 @@ package com.ambertests.aoc.days;
 
 import org.junit.Test;
 
-import static com.ambertests.aoc.days.Day13.Scanner;
-import static org.junit.Assert.*;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
 
 public class Day13Test {
     String[] input = new String[]{
@@ -14,73 +15,27 @@ public class Day13Test {
     };
 
     @Test
-    public void testScannerMove(){
-        Scanner s = new Scanner(3);
-        s.move();
-        assertEquals(1, s.depth);
-    }
-    @Test
-    public void testScannerMoveUp(){
-        Scanner s = new Scanner(3);
-        s.depth = 1;
-        s.increaseDepth = false;
-        s.move();
-        assertEquals(0, s.depth);
-    }
-    @Test
-    public void testScannerMoveBounceBottom(){
-        Scanner s = new Scanner(3);
-        s.depth = 2;
-        s.move();
-        assertEquals(1, s.depth);
-        assertFalse(s.increaseDepth);
-    }
-    @Test
-    public void testScannerMoveBounceTop(){
-        Scanner s = new Scanner(3);
-        s.increaseDepth = false;
-        s.move();
-        assertEquals(1, s.depth);
-        assertTrue(s.increaseDepth);
-    }
-    @Test
-    public void testGetMaxLayer() {
+    public void testParseInput() {
         Day13 day = new Day13();
-        assertEquals(6, day.getMaxLayer(input));
+        HashMap<Integer, Integer> firewall = day.parseInput(input);
+        assertEquals(4, firewall.size());
+        assertEquals(3, firewall.get(0).intValue());
+        assertEquals(2, firewall.get(1).intValue());
+        assertEquals(4, firewall.get(4).intValue());
+        assertEquals(4, firewall.get(6).intValue());
     }
 
     @Test
-    public void testInitFirewall(){
+    public void testGetScore() {
         Day13 day = new Day13();
-        Scanner[] firewall = day.initFirewall(input);
-        assertEquals(7, firewall.length);
-        assertEquals(3, firewall[0].range);
-        assertEquals(2, firewall[1].range);
-        assertEquals(0, firewall[2].range);
-        assertEquals(0, firewall[3].range);
-        assertEquals(4, firewall[4].range);
-        assertEquals(0, firewall[5].range);
-        assertEquals(4, firewall[6].range);
+        HashMap<Integer, Integer> firewall = day.parseInput(input);
+        assertEquals(24, day.getScore(firewall, 0, false));
     }
 
     @Test
-    public void testTraverseFirewall(){
+    public void testFindDelay() {
         Day13 day = new Day13();
-        Scanner[] firewall = day.initFirewall(input);
-        assertEquals(24, day.traverseFirewall(firewall));
-    }
-
-    @Test
-    public void testTraverseFirewallWithDelay(){
-        Day13 day = new Day13();
-        Scanner[] firewall = day.initFirewall(input);
-        assertEquals(0, day.traverseFirewall(firewall, 10, true));
-    }
-
-    @Test
-    public void testFindDelay(){
-        Day13 day = new Day13();
-        Scanner[] firewall = day.initFirewall(input);
+        HashMap<Integer, Integer> firewall = day.parseInput(input);
         assertEquals(10, day.findDelay(firewall));
     }
 
