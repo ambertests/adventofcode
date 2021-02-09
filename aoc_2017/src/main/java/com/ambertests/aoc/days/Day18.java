db5adafd212c4f1b193f8d40447f78147d70cfb5
Amber Race <amber.race@climate.com>
2021-02-09 12:15:05 -0800

    completed Day 18 solution

--- aoc_2017/src/main/java/com/ambertests/aoc/days/Day18.java
+++ aoc_2017/src/main/java/com/ambertests/aoc/days/Day18.java
@@ -2,76 +2,153 @@ package com.ambertests.aoc.days;
 
 import com.ambertests.aoc.common.Day;
 
+import java.util.ArrayDeque;
 import java.util.HashMap;
+import java.util.Queue;
 
 public class Day18 extends Day {
+
+    public static class Program {
+        private final HashMap<String, Long> registers;
+        private final String[] instructions;
+        private final Queue<Long> inbox;
+        private final Queue<Long> outbox;
+        private final boolean soloMode;
+
+        private int currentIndex;
+        private boolean waiting;
+        private boolean terminated;
+
+        public Program(long id, String[] instructions, boolean soloMode) {
+            this.instructions = instructions;
+            this.registers = new HashMap<>();
+            this.registers.put("p", id);
+            this.waiting = false;
+            this.terminated = false;
+            this.inbox = new ArrayDeque<>();
+            this.outbox = new ArrayDeque<>();
+            this.currentIndex = 0;
+            this.soloMode = soloMode;
+        }
+
+        public void receive(long l) {
+            this.inbox.add(l);
+            this.waiting = false;
+        }
+
+        public boolean hasSendItem() {
+            return !this.outbox.isEmpty();
+        }
+
+        public Long getNextSend() {
+            return this.outbox.poll();
+        }
+
+        public boolean canRun() {
+            return !(this.waiting || this.terminated);
+        }
+
+        private long getValue(String str) {
+            if (Character.isLetter(str.charAt(0))) {
+                return this.registers.getOrDefault(str, 0L);
+            } else {
+                return Long.parseLong(str);
+            }
+        }
+
+        public void run() {
+            while (canRun()) {
+                if (this.currentIndex >= this.instructions.length) {
+                    this.terminated = true;
+                    continue;
+                }
+                String[] inst = instructions[this.currentIndex].split(" ");
+                String cmd = inst[0];
+                String x = inst[1];
+                long y = inst.length == 3 ? getValue(inst[2]) : 0;
+                if (Character.isLetter(x.charAt(0))) {
+                    registers.putIfAbsent(x, 0L);
+                }
+                switch (cmd) {
+                    case ("snd"):
+                        this.outbox.add(getValue(x));
+                        this.currentIndex += 1;
+                        break;
+                    case ("set"):
+                        registers.put(x, y);
+                        this.currentIndex += 1;
+                        break;
+                    case ("add"):
+                        registers.put(x, registers.get(x) + y);
+                        this.currentIndex += 1;
+                        break;
+                    case ("mul"):
+                        registers.put(x, registers.get(x) * y);
+                        this.currentIndex += 1;
+                        break;
+                    case ("mod"):
+                        registers.put(x, registers.get(x) % y);
+                        this.currentIndex += 1;
+                        break;
+                    case ("rcv"):
+                        if (soloMode) {
+                            if (registers.get(x) != 0) {
+                                //break the loop
+                                this.currentIndex = instructions.length;
+                            } else {
+                                this.currentIndex += 1;
+                            }
+                        } else {
+                            if (!this.inbox.isEmpty()) {
+                                registers.put(x, this.inbox.poll());
+                                this.currentIndex += 1;
+                            } else {
+                                this.waiting = true;
+                            }
+                        }
+                        break;
+                    case ("jgz"):
+                        if (getValue(x) > 0) {
+                            this.currentIndex += y;
+                        } else {
+                            this.currentIndex += 1;
+                        }
+                }
+            }
+        }
+    }
+
     public Day18() {
         this.dayNum = 18;
     }
 
-    long runProgram(String[] instructions){
-        long lastSound = 0;
-        HashMap<String, Long> registers = new HashMap<>();
-        int index = 0;
-        while(index < instructions.length){
-            String[] inst = instructions[index].split(" ");
-            String cmd = inst[0];
-            String reg = inst[1];
-            long val = inst.length == 3 ? getValue(inst[2], registers) : 0;
-            registers.putIfAbsent(reg, 0L);
-            switch(cmd){
-                case("snd"):
-                    lastSound = registers.get(reg);
-                    index += 1;
-                    break;
-                case("set"):
-                    registers.put(reg, val);
-                    index += 1;
-                    break;
-                case("add"):
-                    registers.put(reg, registers.get(reg) + val);
-                    index += 1;
-                    break;
-                case("mul"):
-                    registers.put(reg, registers.get(reg) * val);
-                    index += 1;
-                    break;
-                case("mod"):
-                    registers.put(reg, registers.get(reg) % val);
-                    index += 1;
-                    break;
-                case("rcv"):
-                    if(registers.get(reg) != 0){
-                        //break the loop
-                        index = instructions.length;
-                    }else{
-                        index += 1;
-                    }
-                    break;
-                case("jgz"):
-                    if(registers.get(reg) > 0){
-                        index += val;
-                    }else{
-                        index += 1;
-                    }
-            
-            }
-
-        }
-        return lastSound;
-    }
-
-    long getValue(String x, HashMap<String, Long> registers){
-        if(Character.isLetter(x.charAt(0))){
-            return registers.getOrDefault(x, 0L);
-        }else{
-            return Long.parseLong(x);
-        }
-    }
-
     @Override
     public void solve() {
-        this.solution1 = runProgram(getInputStringArray());
+        String[] instructions = getInputStringArray();
+
+        //part 1
+        Program p = new Program(0, instructions, true);
+        p.run();
+        while (p.hasSendItem()) {
+            this.solution1 = p.getNextSend();
+        }
+
+        //part 2
+        Program p0 = new Program(0, instructions, false);
+        Program p1 = new Program(1, instructions, false);
+        int p1Sends = 0;
+        while (p0.canRun() || p1.canRun()) {
+            p0.run();
+            while (p0.hasSendItem()) {
+                p1.receive(p0.getNextSend());
+            }
+            p1.run();
+            while (p1.hasSendItem()) {
+                p0.receive(p1.getNextSend());
+                p1Sends += 1;
+            }
+        }
+        this.solution2 = p1Sends;
     }
 
     public static void main(String[] args) {
--- aoc_2017/src/test/java/com/ambertests/aoc/days/Day18Test.java
+++ aoc_2017/src/test/java/com/ambertests/aoc/days/Day18Test.java
@@ -1,24 +1,61 @@
 package com.ambertests.aoc.days;
+
+import com.ambertests.aoc.days.Day18.Program;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+
 public class Day18Test {
+
     @Test
-    public void testRunProgram(){
-        String[] program = {
-            "set a 1",
-            "add a 2",
-            "mul a a",
-            "mod a 5",
-            "snd a",
-            "set a 0",
-            "rcv a",
-            "jgz a -1",
-            "set a 1",
-            "jgz a -2"
+    public void testRunProgram() {
+        String[] instructions = {
+                "set a 1",
+                "add a 2",
+                "mul a a",
+                "mod a 5",
+                "snd a",
+                "set a 0",
+                "rcv a",
+                "jgz a -1",
+                "set a 1",
+                "jgz a -2"
         };
-        Day18 day = new Day18();
-        assertEquals(4, day.runProgram(program));
+        Program p = new Program(0, instructions, true);
+        long val = 0;
+        p.run();
+        while (p.hasSendItem()) {
+            val = p.getNextSend();
+        }
+        assertEquals(4, val);
     }
-    
+
+    @Test
+    public void testRunDualPrograms() {
+        String[] instructions = new String[]{
+                "snd 1",
+                "snd 2",
+                "snd p",
+                "rcv a",
+                "rcv b",
+                "rcv c",
+                "rcv d"
+        };
+        Program p0 = new Program(0, instructions, false);
+        Program p1 = new Program(1, instructions, false);
+        int p1Sends = 0;
+        while (p0.canRun() && p1.canRun()) {
+            p0.run();
+            while (p0.hasSendItem()) {
+                p1.receive(p0.getNextSend());
+            }
+            p1.run();
+            while (p1.hasSendItem()) {
+                p0.receive(p1.getNextSend());
+                p1Sends += 1;
+            }
+        }
+        assertEquals(3, p1Sends);
+    }
+
 }
