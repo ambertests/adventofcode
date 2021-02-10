39bbf5413c1a18c0bb83b5fcce9a5c0e4d54d65a
Amber Race <amber.race@climate.com>
2021-02-09 20:56:57 -0800

    Day 21 solution

---
+++ aoc_2017/src/main/java/com/ambertests/aoc/days/Day21.java
@@ -0,0 +1,161 @@
+package com.ambertests.aoc.days;
+
+import com.ambertests.aoc.common.Day;
+
+import java.util.HashMap;
+
+public class Day21 extends Day {
+    public Day21() {
+        this.dayNum = 21;
+    }
+
+    char[][] flip(char[][] grid) {
+        char[][] flipped = new char[grid.length][];
+        for (int j = 0; j < grid.length; j++) {
+            char[] c = grid[j];
+            char[] reversed = new char[c.length];
+            for (int i = 0; i < c.length; i++) {
+                reversed[c.length - 1 - i] = c[i];
+            }
+            flipped[j] = reversed;
+        }
+        return flipped;
+    }
+
+    char[][] rotate(char[][] grid) {
+        char[][] rotated = new char[grid.length][];
+        for (int r = 0; r < rotated.length; r++) {
+            char[] newRow = new char[grid.length];
+            int c = grid.length - 1 - r;
+            for (int cr = 0; cr < grid.length; cr++) {
+                newRow[cr] = grid[cr][c];
+            }
+            rotated[r] = newRow;
+        }
+        return rotated;
+    }
+
+    String gridToRule(char[][] grid) {
+        StringBuilder sb = new StringBuilder();
+        for (char[] row : grid) {
+            sb.append(row);
+            sb.append("/");
+        }
+        sb.deleteCharAt(sb.lastIndexOf("/"));
+        return sb.toString();
+    }
+
+    long countHashes(char[][] grid) {
+        long hashes = 0;
+        for (char[] r : grid) {
+            for (char c : r) {
+                if (c == '#') {
+                    hashes += 1;
+                }
+            }
+        }
+        return hashes;
+    }
+
+    boolean matchesRule(String rule, char[][] grid) {
+        boolean matches = false;
+        int size = rule.indexOf("/");
+        if (size == grid.length) {
+            long ruleHashes = rule.chars().filter(ch -> ch == '#').count();
+            long gridHashes = countHashes(grid);
+            if (ruleHashes == gridHashes) {
+                for (int i = 0; i < 4; i++) {
+                    if (rule.equals(gridToRule(grid)) || rule.equals(gridToRule(flip(grid)))) {
+                        matches = true;
+                        break;
+                    }
+                    grid = rotate(grid);
+                }
+            }
+        }
+        return matches;
+    }
+
+    char[][] enhancementToGrid(String enhance) {
+        String[] rows = enhance.split("/");
+        char[][] grid = new char[rows.length][];
+        for (int i = 0; i < rows.length; i++) {
+            grid[i] = rows[i].toCharArray();
+        }
+        return grid;
+    }
+
+    void printGrid(char[][] grid) {
+        System.out.println();
+        for (char[] row : grid) {
+            System.out.println(new String(row));
+        }
+    }
+
+    char[][] applyRules(char[][] grid, HashMap<String, String> ruleBook) {
+        int subSize = grid.length % 2 == 0 ? 2 : 3;
+        int subCount = grid.length / subSize;
+        int newSubSize = subSize + 1;
+
+        char[][] newGrid = new char[newSubSize * subCount][];
+        for (int i = 0; i < newGrid.length; i++) {
+            newGrid[i] = new char[newSubSize * subCount];
+        }
+
+        for (int xOffset = 0; xOffset < subCount; xOffset++) {
+            for (int yOffset = 0; yOffset < subCount; yOffset++) {
+                char[][] subGrid = new char[subSize][];
+                for (int sRow = 0; sRow < subSize; sRow++) {
+                    char[] sr = new char[subSize];
+                    for (int sCol = 0; sCol < subSize; sCol++) {
+                        sr[sCol] = grid[sRow + (subSize * xOffset)][sCol + (subSize * yOffset)];
+                    }
+                    subGrid[sRow] = sr;
+                }
+                for (String rule : ruleBook.keySet()) {
+                    if (matchesRule(rule, subGrid)) {
+                        char[][] enhanced = enhancementToGrid(ruleBook.get(rule));
+                        for (int nRow = 0; nRow < newSubSize; nRow++) {
+                            for (int nCol = 0; nCol < newSubSize; nCol++) {
+                                char ch = enhanced[nRow][nCol];
+                                newGrid[nRow + (newSubSize * xOffset)][nCol + (newSubSize * yOffset)] = ch;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return newGrid;
+    }
+
+    @Override
+    public void solve() {
+        char[][] grid = new char[][]{
+                ".#.".toCharArray(),
+                "..#".toCharArray(),
+                "###".toCharArray()
+        };
+        HashMap<String, String> ruleBook = new HashMap<>();
+        for (String s : getInputStringArray()) {
+            String[] kv = s.split(" => ");
+            ruleBook.put(kv[0], kv[1]);
+        }
+        for (int i = 0; i < 5; i++) {
+            grid = applyRules(grid, ruleBook);
+        }
+        this.solution1 = countHashes(grid);
+
+        //do total of 18 updates for part 2
+        for (int i = 0; i < 13; i++) {
+            grid = applyRules(grid, ruleBook);
+        }
+        this.solution2 = countHashes(grid);
+    }
+
+    public static void main(String[] args) {
+        Day21 day = new Day21();
+        day.solve();
+        day.printSolutions();
+    }
+}
---
+++ aoc_2017/src/test/java/com/ambertests/aoc/days/Day21Test.java
@@ -0,0 +1,115 @@
+package com.ambertests.aoc.days;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class Day21Test {
+    char[][] grid = new char[][]{
+            ".#.".toCharArray(),
+            "..#".toCharArray(),
+            "###".toCharArray()
+    };
+
+    @Test
+    public void testFlip(){
+        char[][] expected = new char[][]{
+                ".#.".toCharArray(),
+                "#..".toCharArray(),
+                "###".toCharArray()
+        };
+        Day21 day = new Day21();
+        assertArrayEquals(expected, day.flip(grid));
+    }
+
+    @Test
+    public void testRotate(){
+        char[][] expected = new char[][]{
+                ".##".toCharArray(),
+                "#.#".toCharArray(),
+                "..#".toCharArray()
+        };
+        Day21 day = new Day21();
+        assertArrayEquals(expected, day.rotate(grid));
+    }
+
+    @Test
+    public void testGridToRule(){
+        String expected = ".#./..#/###";
+        Day21 day = new Day21();
+        assertEquals(expected, day.gridToRule(grid));
+    }
+
+    @Test
+    public void testMatchesRule(){
+        String rule = ".#./..#/###";
+        Day21 day = new Day21();
+        assertTrue(day.matchesRule(rule, grid));
+    }
+
+    @Test
+    public void testMatchesRuleFlipped(){
+        String rule = ".#./..#/###";
+        Day21 day = new Day21();
+        assertTrue(day.matchesRule(rule, day.flip(grid)));
+    }
+
+    @Test
+    public void testMatchesRuleRotated(){
+        String rule = ".#./..#/###";
+        Day21 day = new Day21();
+        assertTrue(day.matchesRule(rule, day.rotate(day.rotate(grid))));
+    }
+
+    @Test
+    public void testMatchesRuleRotatedFlipped(){
+        String rule = ".#./..#/###";
+        Day21 day = new Day21();
+        assertTrue(day.matchesRule(rule, day.rotate(day.flip(day.rotate(day.rotate(grid))))));
+    }
+
+    @Test
+    public void testMatchesRule_DiffHashCount(){
+        String rule = ".#./..#/#.#";
+        Day21 day = new Day21();
+        assertFalse(day.matchesRule(rule, grid));
+    }
+
+    @Test
+    public void testMatchesRule_Mismatch(){
+        String rule = ".#./.#./###";
+        Day21 day = new Day21();
+        assertFalse(day.matchesRule(rule, grid));
+    }
+
+    @Test
+    public void testMatchesRule_WrongSize(){
+        String rule = ".#/##";
+        Day21 day = new Day21();
+        assertFalse(day.matchesRule(rule, grid));
+    }
+
+    @Test
+    public void testApplyRules(){
+        HashMap<String, String> ruleBook = new HashMap<>();
+        ruleBook.put("../.#", "##./#../...");
+        ruleBook.put(".#./..#/###", "#..#/..../..../#..#");
+        Day21 day = new Day21();
+        char[][] updated = day.applyRules(grid, ruleBook);
+        assertTrue(day.matchesRule("#..#/..../..../#..#", updated));
+    }
+
+    @Test
+    public void testApplyRulesWithSplit(){
+        HashMap<String, String> ruleBook = new HashMap<>();
+        ruleBook.put("../.#", "##./#../...");
+        ruleBook.put(".#./..#/###", "#..#/..../..../#..#");
+        Day21 day = new Day21();
+        char[][] grid = day.enhancementToGrid("#..#/..../..../#..#");
+        char[][] updated = day.applyRules(grid, ruleBook);
+        assertEquals(6, updated.length);
+        assertEquals(12, day.countHashes(updated));
+
+    }
+}
