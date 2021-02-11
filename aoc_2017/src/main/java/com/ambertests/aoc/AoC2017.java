73e8587d4002e4cdb766323e4344ca25a03490ea
Amber Race <amber.race@climate.com>
2021-02-11 10:54:02 -0800

    File to run all AoC 2017 solutions at once

---
+++ aoc_2017/src/main/java/com/ambertests/aoc/AoC2017.java
@@ -0,0 +1,30 @@
+package com.ambertests.aoc;
+
+import com.google.common.reflect.ClassPath;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Comparator;
+
+public class AoC2017 {
+
+    public static void main(String[] args) throws IOException {
+        ClassPath.from(AoC2017.class.getClassLoader())
+                .getTopLevelClasses("com.ambertests.aoc.days")
+                .stream().map(ClassPath.ClassInfo::load)
+                .sorted(Comparator.comparing(Class::getName))
+                .forEach(c -> {
+                    try {
+                        System.out.println("\n**** " + c.getSimpleName() + " ****");
+                        long start = System.currentTimeMillis();
+                        c.getDeclaredMethod("main", String[].class).invoke(null, (Object) new String[]{});
+                        String time = Long.toString(System.currentTimeMillis() - start) + "ms";
+                        System.out.println("**** Completed in " + time + " ****");
+
+                    } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+}
